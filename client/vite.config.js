cc713cbf816baf08fa1893081375d71b9a8560df
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-04-06 14:42:32 +0300

    vite config vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://email-gemini-l-2-backend.vercel.app",
         secure: false,
       },
     },
