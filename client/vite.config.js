6e97dded394bafdff53f081dd09a099b309f43a3
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-04-06 13:30:50 +0300

    vite config düzenlendi

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://email-gemini-vercell-1.onrender.com",
         secure: false,
       },
     },
